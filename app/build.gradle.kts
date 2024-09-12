97c18a7acf2b96cf5c99b35420a423abed7994f4
TorqueReborn <torquereborn@gmail.com>
2024-09-12 21:03:05 +0530

    Leanback Support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,10 +9,9 @@ android {
     defaultConfig {
         applicationId = "com.ghostreborn.tvtest"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
     }
 
     buildTypes {
@@ -31,7 +30,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.leanback)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
