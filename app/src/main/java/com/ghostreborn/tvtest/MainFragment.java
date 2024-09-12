cffa29c609f804a2f8a3a2b0dbf075babbe9416b
TorqueReborn <torquereborn@gmail.com>
2024-09-12 21:11:23 +0530

    Minimal Leanback Code

---
+++ app/src/main/java/com/ghostreborn/tvtest/MainFragment.java
@@ -0,0 +1,15 @@
+package com.ghostreborn.tvtest;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.leanback.app.BrowseFragment;
+
+public class MainFragment extends BrowseFragment {
+    private static final String TAG = MainFragment.class.getSimpleName();
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
